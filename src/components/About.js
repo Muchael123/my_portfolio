074172621e2e2148f88097f5081c5654a8cf1b16
maich Magodeh <maich@localhost.localdomain>
2023-06-12 11:30:57 +0300

    Improved the about us page to well function

--- src/components/About.js
+++ src/components/About.js
@@ -3,9 +3,9 @@ import { motion } from "framer-motion"
 
 function About() {
   return (
-    <div className="h-screen flex flex-col relative text-center md:text-left max-w-7xl px-10 justify-evenly mx-auto items-center">
-      <h3 className="absolute top-24 uppercase tracking-[20px] text-[#777474] text-2xl">About</h3>
-      <motion.img 
+    <div className="h-screen space-y-10  flex flex-col md:flex-row relative text-center md:text-left max-w-7xl py-20 justify-evenly mx-auto items-center ">
+      <h3 className="absolute top-16 uppercase tracking-[20px] text-[#777474] text-2xl  ">About</h3>
+        <motion.img 
       initial={{
         x:-200,
       }}
@@ -16,7 +16,7 @@ function About() {
       viewport={{once: true,}}
       
       src="./maich.jpg" alt="#"
-       className="mb-20 md:mb-0 xl:ml-0 self-center  mt-20 flex-shrink-0 w-32 h-32 rounded-full  object-cover md:rounded-lg md:w-64 md:h-96 xl:w-[450px] xl:h-[450px]"/>
+       className="-mb-20 mt-20 top-30 md:mb-10 top-42 xl:ml-0 self-center flex-shrink-0 w-56 h-56 rounded-full  object-cover md:rounded-lg md:w-64 md:h-96 xl:w-[450px] xl:h-[450px]"/>
       <motion.div 
        initial={{
         y:200, opacity:0
@@ -26,8 +26,8 @@ function About() {
         y:0, opacity:1
       }}
       viewport={{once: true,}}
-      className='space-y-10 px-0 md:px-10'>
-        <h4 className='text-4xl font-semibold'>Here is a <span classNamey='underline decoration-[#30e42a]'>little </span> about me</h4>
+      className='relative -mb-1 px-2 space-y-0 md:px-10'>
+        <h4 className='text-4xl font-semibold'>Here is a <span className='underline decoration-[#1eff00]'>little </span> about me</h4>
         <p 
         
         className='text-base pb-20'>Welcome to Parrot OS
--- src/components/experience.js
+++ src/components/experience.js
@@ -11,7 +11,7 @@ function Experience() {
         opacity:1
     }}
     transition={{duration:1.5}}
-    className="h-screen flex overflow-hidden relative flex-col text-left md:flex-row max-w-full px-10 justify-evenly mx-auto items-center">
+    className="h-screen flex overflow-hidden relative flex-col text-left md:flex-row max-w-full px-10 justify-evenly mx-auto items-center mt-30">
        <h3 className="absolute top-24 uppercase tracking-[15px] text-[#777474] text-1.5xl ">Experience</h3>
        <div className="w-full flex space-x-7 overflow-x-scroll snap-x snap-mandatory">
         <ECard/>
