a8e9bb69ec0eef0f2f13e575c50382aadc3bff46
maich Magodeh <maich@localhost.localdomain>
2023-06-12 11:35:27 +0300

    Improved the about us button

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -78,7 +78,7 @@ function Hero(){
       duration:1,
      }}
      >
-            <a href="#about" className='px-2 py-5 uppercase md:px-6 border-[#242424] rounded-full text-sm tracking-widest cursor-pointer  transition-all hover:border-[#0ceb04]/40 hover:text-[#0ceb04]/40'>about</a>
+            <a href="#about" className='px-2 py-5 uppercase md:px-6 border-[#242424] rounded-full text-sm tracking-widest cursor-pointer text-gray-500 transition-all hover:border-[#0ceb04]/40 hover:text-[#0ceb04]/40'>about</a>
             <a href='#experience'>
             <button className="px-2 py-2 uppercase md:px-6 border-[#242424] rounded-full text-sm tracking-widest text-gray-500 transition-all hover:border-[#0ceb04]/40 hover:text-[#0ceb04]/40">Experience</button></a>
             <a href="#skills">
