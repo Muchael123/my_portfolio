4d40a746aecfeb190551037ef75d9016ce1cad81
Michael Maina <Mainamichael2849@gmail.com>
2024-11-24 15:31:04 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ function App() {
   return (
     <ThemeContext.Provider value={{ theme, setTheme }}>
       <div
-        className={`flex min-h-screen w-screen flex-col ${
+        className={`flex min-h-screen itmes-center w-screen flex-col ${
           theme === Theme.Dark
             ? "bg-dark-background text-dark-text"
             : "bg-light-background text-gray-800"
